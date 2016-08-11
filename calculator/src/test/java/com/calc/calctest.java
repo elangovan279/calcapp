f1e7086bb4ec64fdba273bff04eeb5038048c50b
elvan <elangovan279@gmail.com>
2016-08-11 16:50:53 +0530

    added test class

---
+++ calculator/src/test/java/com/calc/calctest.java
@@ -0,0 +1,25 @@
+package com.calc;
+
+import org.junit.Assert
+import org.junit.Test;
+
+import com.java.calc;
+
+public class calctest {
+	calc calc = null;
+
+	@Before
+	public void setup() {
+		calc = new Calc();
+	}
+
+	@Test
+	public void add() {
+		int a = 12;
+		int b = 13;
+		int actual = (int) calc.add(a, b);
+		int expected = 25;
+		Assert.assertEquals(expected, actual);
+	}
+
+}
